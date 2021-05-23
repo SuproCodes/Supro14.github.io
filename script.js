7d8e65644608b49e707c9f04b08331f0fe28d515
SuproCodes <dsupratik1@gmail.com>
2021-05-23 13:55:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Python developer", "Front-End Web developer", "KIITiian", "Freelancer"],
+        strings: ["Python developer", "Front-End Web developer", "KIITiian", "Freelancer","Football lover"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Python developer", "Front-End Web developer", "KIITiian", "Freelancer"],
+        strings: ["Python developer", "Front-End Web developer", "KIITiian", "Freelancer","Football lover"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
